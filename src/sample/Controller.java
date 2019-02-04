ec0a5d803ed4b38e0f88b7ec20fc36dca7806b58
Gudilin AC <GudilinAC@gmail.com>
2019-02-04 07:40:18 +0300

    Not working 2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javax.naming.ldap.StartTlsRequest;
+
 public class Controller {
     private View view;
     private SmtpSocket sock = new SmtpSocket();
@@ -12,12 +14,14 @@ public class Controller {
         //System.out.println(new String (Base64.getDecoder().decode("VXNlcm5hbWU6"), StandardCharsets.UTF_8));
 
         sock.connect("smtp.gmail.com");
-        sock.send("EHLO 192.168.100.12\r\n");
-        sock.send("MAIL FROM:<GudilinAC@gmail.com>\r\n");
-        sock.send("RCPT TO:<GudilinAC@gmail.com>\r\n");
-        sock.send("DATA\r\n");
-        sock.send("fdsgdsgdsfds\r\n.\r\n");
-        sock.send("QUIT\r\n");
-
+        sock.send("EHLO 192.168.100.12");
+        sock.send("AUTH LOGIN");
+        sock.sendBase64("GudilinAC@gmail.com");
+        sock.sendBase64("Andrey01");
+        sock.send("MAIL FROM:<GudilinAC@gmail.com>");
+        sock.send("RCPT TO:<GudilinAC@gmail.com>");
+        sock.send("DATA");
+        sock.send("fdsgdsgdsfds\r\n.");
+        sock.send("QUIT");
     }
 }
--- src/sample/SmtpSocket.java
+++ src/sample/SmtpSocket.java
@@ -1,12 +1,15 @@
 package sample;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class SmtpSocket {
-    private Socket socket;
+    private SSLSocket socket;
     private BufferedReader in;
     private BufferedWriter out;
 
@@ -14,7 +17,9 @@ public class SmtpSocket {
     {
         try
         {
-            socket = new Socket(host, 25);
+            socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket("smtp.gmail.com", 465);
+           // socket = SSLSocketFactory.getDefault("smtp.gmail.com", 587);
+            //socket = new Socket(host, 587);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             System.out.println(in.readLine());
@@ -28,14 +33,26 @@ public class SmtpSocket {
         try
         {
             System.out.println(message);
-            out.write(message);
+            out.write(message + "\r\n");
             out.flush();
             System.out.println(in.readLine());
-            //System.out.println(new String (Base64.getDecoder().decode(in.readLine()), StandardCharsets.UTF_8));
             if (in.ready())
                 do {
                     System.out.println(in.readLine());
                 } while (in.ready());
+        }
+        catch (IOException e) { e.printStackTrace(); }
+        return null;
+    }
+
+    public String sendBase64(String message)
+    {
+        try
+        {
+            System.out.println(message + " (Base64)");
+            out.write(Base64.getEncoder().encodeToString(message.getBytes()) + "\r\n");
+            out.flush();
+            System.out.println(in.readLine());
         }
         catch (IOException e) { e.printStackTrace(); }
         return null;
